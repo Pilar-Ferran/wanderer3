5ce34ff36b33b08f32ec5bb2dbe9980a905adf93
pidrizzt <41344256+pidrizzt@users.noreply.github.com>
2022-01-27 16:05:11 +0100

    Add files via upload

--- lib/Screens/explore_screen.dart
+++ lib/Screens/explore_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_login/Component/picture_loading_indicator.dart';
 import 'package:my_login/dataclasses/trip_data.dart';
-import '../Component/trip_preview.dart';
 import '../icons_app.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -27,43 +26,63 @@ class _ExploreScreenState extends State<ExploreScreen> {
 
 
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    resultsLoaded = getUsersPastTripsStreamSnapshots();
-  }
-
-
 
   searchResultsList() {
     var showResults = [];
 
     if(_searchtrip.text != "") {
-      for(var tripSnapshot in _allResults){
-        var title = TripData.fromSnapshot(tripSnapshot).place;
-        if(title.contains(_searchtrip.text)) {
-          TripData aux= TripData.fromSnapshot(tripSnapshot);
+      for(var doc in _allResults){
 
-          showResults.add(aux);
+        var docData = doc.data() as Map<String, dynamic>;
+        TripData trip = TripData.fromJson(docData);
+
+
+        trip.firestorePath = doc.reference.path;
+        //trip.firestoreId = doc.reference.id;
+
+        //we add the preview image, which is stored in a different Firebase service.
+        trip.previewPicFuture = getImage(trip.previewPic);
+
+        //var title = TripData.fromSnapshot(tripSnapshot).place;
+        //if(title.contains(_searchtrip.text)) {
+          //TripData aux= TripData.fromSnapshot(tripSnapshot);
+
+          showResults.add(trip);
         }
-      }
-
-    } else {
-
+      } else {
       showResults = List.from(_allResults);
     }
     setState(() {
       _resultsList = showResults;
+      loading2=false;
+      print(_resultsList);
+
     });
+    if(_resultsList.length==0){
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('No trips found', style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),),
+          backgroundColor: Colors.red,
+          action: SnackBarAction(label: 'Try again', textColor: Colors.white ,onPressed: ScaffoldMessenger.of(context).hideCurrentSnackBar),
+        ),
+      );
+    }
   }
 
   getUsersPastTripsStreamSnapshots() async {
+    setState(() {
+      loading2 = true;
+    });
     var data = await FirebaseFirestore.instance.collection('trips').where("location", isEqualTo: _searchtrip.text).get();
     setState(() {
       _allResults = data.docs;
     });
     searchResultsList();
-    print(_allResults);
+
   }
   @override
 
@@ -94,6 +113,16 @@ class _ExploreScreenState extends State<ExploreScreen> {
         ),
         body: TabBarView(
           children: [
+            loading2
+                ? Column(
+              children: [
+                SizedBox(
+                  height: size.height / 3,
+                ),
+                const Center(
+                  child: CircularProgressIndicator(),
+                ),],)
+                :
             Container(
               constraints: const BoxConstraints.expand(),
               decoration: const BoxDecoration(
@@ -144,14 +173,45 @@ class _ExploreScreenState extends State<ExploreScreen> {
                       SizedBox(
                         height: size.height / 30,
                       ),
-                      Expanded(
-                              child: ListView.builder(
-                              itemCount: _resultsList.length,
-                              itemBuilder: (BuildContext context, int index) =>
-                              TripPreview(_resultsList[index]),
-                              )
-
-                      ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _resultsList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Container(
+                          color: Colors.white,
+                          child:ListTile(
+                          onTap:() {},
+                          leading: FutureBuilder(
+                            future: getImage(_resultsList[index].previewPic),
+                            builder: (context, snapshot){
+                              if (snapshot.connectionState == ConnectionState.done) {
+                                if (snapshot.hasError) {
+                                  print("error in snapshot: "+snapshot.error.toString());
+                                  return const Text("error");
+                                }
+                                else {
+                                  return Image.network(snapshot.data as String);
+                                }
+                              }
+                              else { //show loading
+                                return const PictureLoadingIndicator();
+                              }
+                            },),
+                          selectedTileColor: Colors.white,
+                          title: Text(
+                            _resultsList[index].title,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          subtitle: Text(_resultsList[index].authorUser),
+                          trailing: Icon(Icons.account_balance, color: Colors.black),
+                        ),),); }
+                ),
+                    ),
     ],
                   ),
 
@@ -310,7 +370,6 @@ class _ExploreScreenState extends State<ExploreScreen> {
     var url = await ref.getDownloadURL();
     return url;
   }
-
 
 
 
