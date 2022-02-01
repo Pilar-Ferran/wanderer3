de1fcbf9e744c331d160f779c9c54fdc77b5b677
Ferran00 <ferran.iglesias.barenys@estudiantat.upc.edu>
2022-02-01 20:02:34 +0100

    install the test thing and 1st attempt at unit test

---
+++ test/create_trip_screen_test.dart
@@ -0,0 +1,13 @@
+// Import the test package and Counter class
+/*import 'package:my_login/Screens/create_trip_screen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final createTripScreen = CreateTripScreen();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+}*/
